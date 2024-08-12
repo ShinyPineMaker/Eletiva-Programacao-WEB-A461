30113023de2a29319baa36b43db02e92a30ce5c4
ShinyPineMaker <guizin.lps@gmail.com>
2024-08-12 15:01:22 -0300

    update functions update index add form bonusanual e form salariosemanal

--- Exercicios-Revisao/Index.php
+++ Exercicios-Revisao/Index.php
@@ -1,3 +1,5 @@
+<?php require "functions.php"; ?>
+
 <!DOCTYPE html>
  <html lang="en">
  <head>
@@ -11,23 +13,81 @@
         <p>Calcular Diferença entre Horários: </p>
         <div>
             <label for="entrada">Horário de Entrada: </label>
-            <input type="number" name="hrentrada" id="hr_entrada">
+            <input type="number" name="hrentrada">
         </div>
         <div>
             <label for="saida">Horário de Saída</label>
-            <input type="number" name="hrsaida" id="hr_saida">
+            <input type="number" name="hrsaida">
         </div>
         <input type="submit" value="enviar horário">
     </form>
     
     <?php
-        require "functions.php";
-        if ($_POST['hrentrada'] and $_POST['hrsaida']){
+        if (!empty($_POST['hrentrada']) and !empty($_POST['hrsaida'])){
             echo "<p>", Diferenca($_POST['hrentrada'], $_POST['hrsaida']), "</p>";
         }
         else{
             echo "";
         }
+    ?>
+
+    <form name="form[salariosemanal]" action="Index.php" method="POST">
+        <p>Calcular salário semanal: </p>
+        <div>
+            <label for="horastrabalhadas">Horas Trabalhadas: </label>
+            <input type="number" name="horastrabalhadas">
+        </div>
+        <div>
+            <label for="valorhora">Valor da Hora: </label>
+            <input type="number" name="valorhora">
+        </div>
+
+        <input type="submit" value="calcular salário">
+    </form>
+
+    <?php
+        if (!empty($_POST['horastrabalhadas']) and !empty($_POST['valorhora'])){
+            echo "<p>", SalarioMensal($_POST['horastrabalhadas'], $_POST['valorhora']), "</p>";
+        }
+        else {
+            echo "";
+        }
+    ?>
+
+    <form name="form[bonusanual]" action="Index.php" method="POST">
+        <p>Distribuir bônus anual: </p>
+        <div>
+            <label for="lucros">Lucros da Empresa: </label>
+            <input type="number" name="lucros">
+        </div>
+        <div>
+            <label for="nome_funcionario">Nome do Funcionário: </label>
+            <input type="text" name="nomefuncionario">
+        </div>
+        <div>
+            <p>Escala de Recebimento do Lucro: </p>
+            <div>
+                <label for="1">1</label>
+                <label for="2">2</label>
+                <label for="3">3</label>
+                <label for="4">4</label>
+                <label for="5">5</label>
+            </div>
+            <div>
+                <input type="radio" name="escala" value="1">
+                <input type="radio" name="escala" value="2">
+                <input type="radio" name="escala" value="3">
+                <input type="radio" name="escala" value="4">
+                <input type="radio" name="escala" value="5">
+            </div>
+        </div>
+
+        <input type="submit" value="distribuir bônus">
+    </form>
+
+    <?php
+        if (!empty($_POST['lucros']) and !empty($_POST['escala']))
+        echo "<p>", DistribuirBonusAnual($_POST['lucros'], $_POST['escala']), "</p>";
     ?>
 
  </body>
--- Exercicios-Revisao/functions.php
+++ Exercicios-Revisao/functions.php
@@ -1,6 +1,31 @@
 <?php
 
-function Diferenca($x, $y){
-    $r = $y - $x;
-    return $r; 
+function Diferenca($hora_entrada, $hora_saida){
+    return $hora_saida - $hora_entrada; 
+}
+
+function SalarioMensal($quantidade_horas, $valor_hora){
+    return ($quantidade_horas * $valor_hora) / 4.5;
+}
+
+function DistribuirBonusAnual($lucro, $fatorlucro){
+    switch ($fatorlucro){
+        case 1:
+            $fatorlucro=0.1;
+            break;
+        case 2:
+            $fatorlucro=0.25;
+            break;
+        case 3:
+            $fatorlucro=0.4;
+            break;
+        case 4:
+            $fatorlucro=0.55;
+            break;
+        case 5:
+            $fatorlucro=0.7;
+            break;            
+    }
+
+    return $lucro * $fatorlucro;
 }
