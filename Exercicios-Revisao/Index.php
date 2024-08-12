8bb6b859397ec03b2b6c8bdcbc3bd2079eeba855
ShinyPineMaker <guizin.lps@gmail.com>
2024-08-12 14:05:48 -0300

    update index.php

--- Exercicios-Revisao/Index.php
+++ Exercicios-Revisao/Index.php
@@ -9,17 +9,24 @@
  <body>
     <form name="form[diferencahora]" action="Index.php" method="POST">
         <p>Calcular Diferença entre Horários: </p>
-        <label for="entrada">Horário de Entrada: </label>
-        <input type="number" name="entrada" id="hr_entrada">
-        <label for="saida">Horário de Saída</label>
-        <input type="number" name="saida" id="hr_saida">
+        <div>
+            <label for="entrada">Horário de Entrada: </label>
+            <input type="number" name="hrentrada" id="hr_entrada">
+        </div>
+        <div>
+            <label for="saida">Horário de Saída</label>
+            <input type="number" name="hrsaida" id="hr_saida">
+        </div>
         <input type="submit" value="enviar horário">
     </form>
     
     <?php
         require "functions.php";
-        if ($_POST){
-            echo Diferenca($_POST['entrada'], $_POST['saida']);
+        if ($_POST['hrentrada'] and $_POST['hrsaida']){
+            echo "<p>", Diferenca($_POST['hrentrada'], $_POST['hrsaida']), "</p>";
+        }
+        else{
+            echo "";
         }
     ?>
 
