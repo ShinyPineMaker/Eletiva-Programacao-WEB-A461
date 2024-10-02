3bae3e6e91210aeabb73971cac579e9cbc45ab6c
ShinyPineMaker <guizin.lps@gmail.com>
2024-10-02 17:23:24 -0300

    finished lista 2

--- ListaExercicios2/Exercicio1/Ponto.php
+++ ListaExercicios2/Exercicio1/Ponto.php
@@ -12,6 +12,11 @@ class Ponto{
         $this->y = $y;
         self::$qtdObjetosCriados ++;
     }
+    
+
+    public function Contagem(){
+        return self::$qtdObjetosCriados;
+    }
 
     public function getX(){
         return $this->x;
@@ -29,19 +34,15 @@ class Ponto{
         $this->y = $value;
     }
 
-    public function Contagem(){
-        return self::$qtdObjetosCriados;
+    public function deslocar($dx, $dy){
+        return sqrt((($dx->getX() - $this->x) ** 2) + (($dy - $this->y) ** 2));
     }
 
-    public function CalcularDistanciaPonto(Ponto $alvo){
-        return sqrt((($alvo->x - $this->x) ** 2) + (($alvo->y - $this->y) ** 2));
+    public function DistanciaEntrePontosObjeto(Ponto $ponto1, Ponto $ponto2){
+        return sqrt((($ponto2->getX() - $ponto1->getX()) ** 2) + (($ponto2->getY() - $ponto1->getY()) ** 2));
     }
-
-    public function CalcularDistanciaCoord($alvoX, $alvoY){
-        return sqrt((($alvoX - $this->x) ** 2) + (($alvoY - $this->y) ** 2));
-    }
-
-    public function CalcularDistancia($x1, $y1, $x2, $y2){
-        return sqrt((($x2 - $x1) ** 2) + (($y2 - $y1) ** 2));
+    
+    public function DistanciaEntrePontosCoordenada(int $x1, int $y1, int $x2, int $y2){
+        return sqrt( (($x2 - $x1) ** 2) + (($y2 - $y1) ** 2) ); 
     }
 }
